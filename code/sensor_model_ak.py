0e25a1c7af21dea1835ad21c88543d6ea1e1b0a3
Prasanna <pkettava@andrew.cmu.edu>
2022-02-14 22:53:47 -0500

    Sensor Model cleaned up and working

--- code/sensor_model_ak.py
+++ code/sensor_model_ak.py
@@ -36,11 +36,11 @@ class SensorModel:
         # Used in p_max and p_rand, optionally in ray casting
         self._max_range = 8183
 
-
         # Used in sampling angles in ray casting
-        self._subsampling = 1
+        self._subsampling = 10
 
         self.occupancy_threshold = 1E-7
+        self.probability_min = 0.4
         self.visualization = True
         self.plot_measurement = True
 
@@ -55,68 +55,36 @@ class SensorModel:
         robot_x, robot_y, robot_theta = x_t1
         
         temp = self.map[min(int(robot_y/10.), 799)][min(int(robot_x/10.), 799)]
-        if temp > 0.4 or temp == -1:
+        if temp > self.probability_min or temp == -1:
             return 1e-100
 
-        # laser_x = int(round((robot_x + (25 * np.cos(robot_theta))))/10)
-        # laser_y = int(round((robot_y + (25 * np.sin(robot_theta))))/10)
+        laser_x = int(round((robot_x + (25 * np.cos(robot_theta)))/10.0))
+        laser_y = int(round((robot_y + (25 * np.sin(robot_theta)))/10.0))
 
-        temp_x = 25.0 * np.cos(robot_theta)
-        temp_y = 25.0 * np.sin(robot_theta)
-        laser_x = int(round((robot_x + temp_x) / 10.0))
-        laser_y = int(round((robot_y + temp_y) / 10.0))
-
-        for k in range(-90,90,10): #here K is 180
-            
+        for k in range(-90,90,self._subsampling): #here K is 180
+    
             z_kt = z_t1_arr[k + 90]
 
-            #z_kt_star = self.rayCast(k, robot_theta, laser_x, laser_y)
             z_kt_star = self.ray_cast( laser_x, laser_y, robot_theta, k)
-            prob1 = (self._z_hit * self.p_hit(z_kt_star,z_kt))
-            prob2 = (self._z_short * self.p_short(z_kt_star,z_kt)) 
-            prob3 = (self._z_max * self.p_max(z_kt)) 
-            prob4 = (self._z_rand * self.p_rand(z_kt))
-            prob = prob1 + prob2 + prob3 + prob4
+            
+            prob = (self._z_hit * self.p_hit(z_kt_star,z_kt)) + (self._z_short * self.p_short(z_kt_star,z_kt)) + (self._z_max * self.p_max(z_kt)) + (self._z_rand * self.p_rand(z_kt))
             if prob > 0:
                 prob_zt1 = prob_zt1 + np.log(prob)
         return math.exp(prob_zt1)
 
     def ray_cast(self, origin_x, origin_y, robot_theta, step):
 
-        z_kt_star = 0
-
         x_rough = origin_x
         y_rough = origin_y
-        x = origin_x
-        y = origin_y
+        x_int = origin_x
+        y_int = origin_y
         theta = robot_theta + math.radians(step) 
-        while 0 < x < self.map.shape[1] and 0 < y < self.map.shape[0] and (abs(self.map[y,x]) < self.occupancy_threshold):
+        while 0 < x_int < self.map.shape[1] and 0 < y_int < self.map.shape[0] and (abs(self.map[y_int,x_int]) < self.occupancy_threshold):
             x_rough += 2*np.cos(theta)
             y_rough += 2*np.sin(theta)
-            x = int(round(x_rough))
-            y = int(round(y_rough))
-        end_p = np.array([x,y])
-        start_p = np.array([origin_x,origin_y])
-        dist = np.linalg.norm(end_p-start_p) * 10
-
-        return dist
-
-
-    def rayCast(self, deg, ang, coord_x, coord_y):
-        # print("Ray casting from: ", coord_x, coord_y)
-        final_angle= ang + math.radians(deg)
-        start_x = coord_x
-        start_y = coord_y
-        final_x = coord_x
-        final_y = coord_y
-        while 0 < final_x < self.map.shape[1] and 0 < final_y < self.map.shape[0] and abs(self.map[final_y, final_x]) < 0.0000001:
-            start_x += 2 * np.cos(final_angle)
-            start_y += 2 * np.sin(final_angle)
-            final_x = int(round(start_x))
-            final_y = int(round(start_y))
-        end_p = np.array([final_x,final_y])
-        start_p = np.array([coord_x,coord_y])
-        dist = np.linalg.norm(end_p-start_p) * 10
+            x_int = int(round(x_rough))
+            y_int = int(round(y_rough))
+        dist = math.sqrt((x_int-origin_x)**2 + (y_int-origin_y)**2) * 10
         return dist
 
 
